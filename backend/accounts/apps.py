37137d8e49f459137a7591bca409d422fe2155ef
DaphneDana <nakamyukadaphne62@gmail.com>
2026-03-24 12:46:26 +0300

    modified the accounts app

--- backend/accounts/apps.py
+++ backend/accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+""" this is the apps page """
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
